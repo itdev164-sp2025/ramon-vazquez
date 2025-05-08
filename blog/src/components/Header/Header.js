6c72979b885c0f28f947912fbd5ddfb136897437
Ramon Vazquez <vazquerl@gmatc.matc.edu>
2025-05-08 14:09:00 -0500

    Create Heading1 Component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,25 +2,29 @@ import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+import { H1 } from "../Heading"
+
 const StyledHeader = styled.header`
-      margin: 0 auto;
-      padding: var(--space-4) var(--size-gutter);
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      background: ${props => props.theme.header.backgroundColor}
-    `
+  margin: 0 auto;
+  padding: var(--space-4) var(--size-gutter);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: ${props => props.theme.header.backgroundColor}
+`
 
 const StyledLink = styled(Link)`
-        font-size: var(--font-sm);
-        text-decoration: none;
-        color: ${props => props.theme.header.color}
-      `
+  font-size: var(--font-sm);
+  text-decoration: none;
+  color: ${props => props.theme.header.color}
+`
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
     <StyledLink to="/">
-      {siteTitle}
+      <H1>
+        {siteTitle}
+      </H1>
     </StyledLink>
   </StyledHeader>
 )
